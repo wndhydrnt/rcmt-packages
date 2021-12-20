b10f39a597b2637bba37bb44d09727a971719c71
wndhydrnt <hydrantanderwand@gmail.com>
2021-12-20 19:34:00 +0100

    Execute rcmt via its own entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 pip install -qq poetry
-rcmt run --config /opt/work/rcmt.yaml --packages /opt/work/packages /opt/work/run.py
+/opt/entrypoint.sh run --config /opt/work/rcmt.yaml --packages /opt/work/packages /opt/work/run.py
